e137eeff0baa44b734e1683deab1e6cdfa9fd9e0
Punam <punamben211@gmail.com>
2022-07-04 23:23:52 +0530

    Add project files.

---
+++ Common.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace CheckParantesisAssessment
+{
+    public static class Common
+    {
+        public static bool IsMatchingPair(char char1, char char2)
+        {
+            if (char1 == '(' && char2 == ')')
+                return true;
+            else if (char1 == '{' && char2 == '}')
+                return true;
+            else if (char1 == '[' && char2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+        public static bool AreBracketsBalanced(char[] exp)
+        {
+            var st = new Stack<char>();
+
+            for (var i = 0; i < exp.Length; i++)
+            {
+                if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                    st.Push(exp[i]);
+
+                if (exp[i] == '}' || exp[i] == ')'
+                    || exp[i] == ']')
+                {
+
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    else if (!IsMatchingPair(st.Pop(), exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (st.Count == 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CheckParantesisAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter input string..:");
+            var inputStr = Console.ReadLine();
+            var charArr = inputStr.ToCharArray();
+
+            if (Common.AreBracketsBalanced(charArr))
+                Console.WriteLine("All parenthesis are balanced");
+            else
+                Console.WriteLine("Not balanced");
+        }
+    }
+}
